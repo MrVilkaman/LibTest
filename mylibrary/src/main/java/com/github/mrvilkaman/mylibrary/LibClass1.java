2ac13e34a2c36803fba538247abe3071a4a36863
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-12-25 18:06:05 +0300

    init

---
+++ mylibrary/src/main/java/com/github/mrvilkaman/mylibrary/LibClass1.java
@@ -0,0 +1,7 @@
+package com.github.mrvilkaman.mylibrary;
+
+
+public class LibClass1 {
+
+	public static final String MY_TAG = "LibClass1";
+}
---
+++ mylibrary2/src/main/java/com/github/mrvilkaman/mylibrary2/LibClass2.java
@@ -0,0 +1,7 @@
+package com.github.mrvilkaman.mylibrary2;
+
+
+public class LibClass2 {
+	public static final String MY_TAG = "LibClass1";
+
+}
